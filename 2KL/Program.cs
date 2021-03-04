e76f885cd77e186dc928ca72824493145cf95b90
klykastik2000@gmail.com <klykastik2000@gmail.com>
2021-03-04 20:19:39 +0600

    Добавьте файлы проекта.

---
+++ 2KL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2KL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
