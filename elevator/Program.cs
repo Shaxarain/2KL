f0e6963621310af5001142e70a586a684a656faa
klykastik2000@gmail.com <klykastik2000@gmail.com>
2021-03-09 05:21:54 +0600

    Ну вроде бы готово...

--- elevator/Program.cs
+++ elevator/Program.cs
@@ -6,22 +6,23 @@ namespace elevator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Поздравляю! Перед тобой первый (и скорее всего последний) клавиатурный лифт!\nТеперь слушай инструктаж.\nКоманда open откроет дверь, close закроет, если есть возможность.\nКоманда go запустит движение, stop... ну ты понял.\nУ тебя есть право выбора и во время движения, но не делай глупостей.\nКоманда end значит, что ты накатался и пора валить.\nАх, да. Я не катаю тех, кто даёт неверные команды.\nТак где тебя подобрать?");
-            Elevator.go();
+            Console.WriteLine("Поздравляю! Перед тобой первый (и скорее всего последний) клавиатурный лифт!\nТеперь слушай инструктаж.\nКоманда open открывает двери, close - закрывает, если есть возможность.\nКоманда go запустит движение.\ncomeon - впустит новых пассажиров, goaway - позволит выйти.\nУ тебя есть право выбора и во время движения, но не делай глупостей.\nКоманда end значит, что ты накатался и пора валить.\nАх, да. Я не катаю тех, кто даёт неверные команды.\nТак где тебя подобрать?");
+            Elevator.start();
         }
         public class Elevator
         {
             static int floor = 25; //кол-во этажей.
-            static int startfloor = 1;
-            static int endfloor;
+            static int startfloor = 1; //этаж, на котором стоит лифт.
+            static int endfloor; //куда надо попасть.
             static int carrying = 5; //грузоподъёмность в людях (не больше 5 челов).
+            static int alotof = 0;
             static bool opendoor = true; // состояние дверей true = открыты.
             static bool work = true; //Чтобы свалить.
             public static void uroboros()
             {
                 do
                 {
-                    Elevator.control();
+                    control();
                 } while (work != false);
             }
             public static void control()
@@ -31,7 +32,7 @@ namespace elevator
                 switch (choice)
                 {
                     case "go":
-                        Elevator.go();
+                        start();
                         break;
                     case "end":
                         end();
@@ -40,9 +41,45 @@ namespace elevator
                         Console.WriteLine("Оу, эм... пожалуйста.");
                         control();
                         break;
+                    case "open":
+                        open();
+                        break;
+                    case "close":
+                        close();
+                        break;
+                    case "comeon":
+                        comeon();
+                        break;
+                    case "goaway":
+                        goaway();
+                        break;
                 }
             }
-            public static void go()
+            public static void open()
+            {
+                if (opendoor == true)
+                {
+                    Console.WriteLine("[Двери открыты]");
+                    control();
+                }
+                else
+                {
+                    Console.WriteLine("Эй, чё делаешь. Я тут еду вообще-то.[Двери не открылись]");
+                }
+            }
+            public static void close()
+            {
+                if (opendoor == false)
+                {
+                    Console.WriteLine("Двери итак закрыты...");
+                }
+                else
+                {
+                    Console.WriteLine("Двери закрылись.");
+                    control();
+                }
+            }
+            public static void start()
             {
                 Console.WriteLine("На какой этаж пожаловать? Если чё их всего 25.");
                 endfloor = Convert.ToInt32(Console.ReadLine());
@@ -54,30 +91,65 @@ namespace elevator
                 else
                 {
                     Console.WriteLine("Как скажешь, мешок с костями...");
-                    move();
+                    comeon();
                 }
             }
-            public static void weight()
+            public static void comeon()
             {
-                Console.WriteLine("Кстати, а сколько вас? Больше 5 не беру.");
-                int y = Convert.ToInt32(Console.ReadLine());
-                if (y > carrying)
+                if (opendoor == true)
                 {
-                    Console.WriteLine("Я лифт, а не грузоподъёмный кран! Кыш!");
-                    end();
-                }
-                else if (y == carrying)
-                {
-                    Console.WriteLine("Прям впритык. Надеюсь, канат выдержит...");
+                    Console.WriteLine("А сколько вас? Больше 5 не беру.");
+                    alotof += Convert.ToInt32(Console.ReadLine());
+                    if (alotof > carrying)
+                    {
+                        Console.WriteLine("Я лифт, а не грузоподъёмный кран! Кыш!");
+                        end();
+                    }
+                    else if (alotof == carrying)
+                    {
+                        Console.WriteLine("Прям впритык. Надеюсь, канат не выдержит...");
+                        move();
+                    }
+                    else
+                    {
+                        Console.WriteLine("На легке!");
+                        move();
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("На легке!");
+                    Console.WriteLine("Я на ходу людишек не подбираю!");
+                }
+            }
+            public static void goaway()
+            {
+                Console.WriteLine("Сколько выходит?");
+                alotof -= Convert.ToInt32(Console.ReadLine());
+                if (opendoor == true)
+                {
+                    if (alotof < 0)
+                    {
+                        Console.WriteLine("Слышь... Ты думаешь раз я лифт, что считать не умею?!!");
+                        end();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ну и счастливого пути...");
+                        if (alotof != 0)
+                        {
+                            control();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Чё, прям на ходу хотите выйти? Регламент запрещает так делать...");
                 }
             }
             public static void move()
             {
-                Console.WriteLine("Мы начинаем движение. Ты можешь поменять этаж или ехать куда хотел - просто жми enter.");
+                opendoor = false;
+                Console.WriteLine("Мы начинаем движение. Ты можешь поменять этаж или продолжить движение - просто жми enter.");
                 if (startfloor < endfloor)
                 {
                     for(int i = Elevator.startfloor; startfloor!=endfloor; startfloor++)
@@ -98,11 +170,12 @@ namespace elevator
                     Console.WriteLine("Ой какое совпадение, а я уже тут.");
                 }
                 Console.WriteLine("Лифт на {0} этаже. Мы на месте.", startfloor);
+                opendoor = true;
                 control();
             }
             public static void end()
             {
-                Console.WriteLine("[Двери лифта открылись на первом попавшемся же этаже и механическая рука вытолкнула Вас прочь.]");
+                Console.WriteLine("[Лифт самоуничтожился]");
             }
         }
     }
