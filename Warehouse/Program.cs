d5b40f45bfc68e53fafa899c03e5e9ffceb28d63
klykastik2000@gmail.com <klykastik2000@gmail.com>
2021-03-12 05:40:52 +0600

    more products

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -8,19 +8,35 @@ namespace Warehouse
         static void Main(string[] args)
         {
             Employee Archi = new Employee("Arthur", "boss");
+            Employee Ch = new Employee("Cheburek", "Cooker");
 
             Warehouse HouseofLiquid = new Warehouse("Privet Drive", 500, true);
             Warehouse HouseofObject = new Warehouse("Forest", 1000, false);
             Warehouse HouseofGrit = new Warehouse("Sandbox", 333, false);
 
             IProduct poison = new LiquidProduct("poison", "000", 666);
+            IProduct aj = new LiquidProduct("apple juice", "010", 560);
+            IProduct bj = new LiquidProduct("berry juice", "011", 600);
+
             IProduct sugar = new GritProduct("sygar", "100", 500);
+            IProduct cocaine = new GritProduct("coconut flakes", "101", 3000);
+            cocaine.description = "Not a cocaine";
+
+            IProduct heart = new ObjectProduct("heart of human", "200", 5000);
+            IProduct mouse = new ObjectProduct("dead mouse","201", 0);
 
             string AdResult1 = HouseofLiquid.Adding(sugar);
+            HouseofLiquid.Adding(aj);
+            HouseofLiquid.Adding(bj);
             Console.WriteLine(AdResult1);
 
             string AdResult2 = HouseofObject.Addresp_emp(Archi);
             Console.WriteLine(AdResult2);
+
+            //find products by SKU
+
+
+            //sum all warehouse
 
             /*foreach (IProduct lel in HouseofLiquid.products)
             {
